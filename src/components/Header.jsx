72606ea6dd7b1fedb99731a22403acf8c8d2c0e1
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-06-02 23:31:32 +0600

    Маршрутизация между главной страницей и корзиной

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logoSvg from "../assets/img/pizza-logo.svg"
+import {Button} from "./index";
+import {Link} from "react-router-dom";
+
+const Header = props => {
+    return (<div className="header">
+    <div className="container">
+        <Link to="/">
+            <div className="header__logo">
+                <img width="38" src={logoSvg} alt="Pizza logo"/>
+                <div>
+                    <h1>React Pizza</h1>
+                    <p>самая вкусная пицца во вселенной</p>
+                </div>
+            </div>
+        </Link>
+
+        <div className="header__cart">
+            <Link to={"/cart"}>
+                <Button/>
+            </Link>
+        </div>
+    </div>
+        </div>
+    )
+}
+export default Header
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {default as Categories} from "./Categories";
 export {default as Button} from "./Button";
 export {default as SortPopup} from "./SortPopup";
 
+
