76ec7e3153c6b03b9f48ba490b9983da927e2b9e
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-06-02 09:49:54 +0600

    App, Header, Button

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logoSvg from "./assets/img/pizza-logo.svg"
+import {Button} from "./Button";
+
+export const Header = props => {
+    return (<div className="header">
+    <div className="container">
+        <div className="header__logo">
+            <img width="38" src={logoSvg} alt="Pizza logo"/>
+            <div>
+                <h1>React Pizza</h1>
+                <p>самая вкусная пицца во вселенной</p>
+            </div>
+        </div>
+        <div className="header__cart">
+         <Button />
+        </div>
+    </div>
+</div>
+)}
