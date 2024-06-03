f131de257e27767a1df7d088366b9a99069df427
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-06-03 14:56:28 +0600

    Запрос элементов для отрисовки на странице.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Header} from "./components";
 import {Home, Cart} from "./pages";
 import {Route, Routes} from "react-router";
 
 
 function App() {
+    const [pizzas, setPizzas] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/db.json')
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                 setPizzas(json.pizzas);
+                 ;
+            })
+
+    }, []);
+    console.log(pizzas)
   return (
       <div className="wrapper">
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home items={pizzas} />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
 
