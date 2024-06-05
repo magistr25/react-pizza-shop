5156921fee1f1b9b2dab761ace3f176dbe3938db
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-06-05 14:28:01 +0600

    Поправила баг из-за REDUX_DEVTOOLS

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,9 +5,12 @@ import {thunk} from "redux-thunk";
 
 
 
-const composeEnhancers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
 
 window.store = store; // Для отладки, чтобы иметь доступ к хранилищу в консоли
 export default store;
